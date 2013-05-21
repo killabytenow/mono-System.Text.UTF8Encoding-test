67bf43dfc472cfd355b1a0df4ee676e13e71a3c5
Gerardo García Peña <killabytenow@gmail.com>
2013-05-21 19:06:35 +0200

    check only changed files

--- mkdiff.sh
+++ mkdiff.sh
@@ -3,19 +3,36 @@
 MONO="cnvout-mono"
 MNET="cnvout-other"
 DIFF="cnvout-diff"
-rm -rf "$DIFF/"
 mkdir -p "$DIFF/"
 for i in "$MONO/"*.txt; do
         F="`basename "$i"`"
         M="Checking file '$F'..."
         echo -n "$M\r"
-        H1="`md5sum "$MONO/$F"`"
-        H2="`md5sum "$MNET/$F"`"
-        if [ "$H1" != "$H2" ]; then
-          echo -n "`echo "$M" | sed 's#.# #g'`\r"
+        if [ \(                                 \
+               ! -e "$DIFF/.ok-$F"              \
+               -o "$MONO/$F" -nt "$DIFF/.ok-$F" \
+               -o "$MNET/$F" -nt "$DIFF/.ok-$F" \
+             \) -a \(                           \
+               ! -e "$DIFF/$F"                  \
+               -o "$MONO/$F" -nt "$DIFF/$F"     \
+               -o "$MNET/$F" -nt "$DIFF/$F"     \
+             \) ]; then
+          rm -f -- "$DIFF/.ok-$F" "$DIFF/$F"
+          if [ -e "$MONO/$F" -a -e "$MNET/$F" ]; then
+            H1="`md5sum "$MONO/$F"`"
+            H2="`md5sum "$MNET/$F"`"
+            if [ "$H1" != "$H2" ]; then
+              echo -n "`echo "$M" | sed 's#.# #g'`\r"
+              echo "ok!" > "$DIFF/.ok-$F"
+            else
+              echo "Differences found in file '$F'."
+              diff -ur "$MONO/$F" "$MNET/$F" > "$DIFF/$F"
+            fi
+          else
+            echo "File '$F' does not exists on both platforms."
+          fi
         else
-          echo "Differences found in file '$F'."
-          diff -ur "$MONO/$F" "$MNET/$F" > "$DIFF/$F"
+          echo -n "`echo "$M" | sed 's#.# #g'`\r"
         fi
 done
 
